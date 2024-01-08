c78eb4c7b1951b30ac4310aa6249d9e76898ee4a
natali.r <natasha-razdobreeva@mail.ru>
2024-01-08 09:42:22 +0300

    restMonthCounter

---
+++ src/main/java/ru/netology/restMonthCounter/services/Main.java
@@ -0,0 +1,8 @@
+package ru.netology.restMonthCounter.services;
+
+public class Main {
+    public static void main(String[] args) {
+        calculateservices service = new calculateservices();
+        System.out.println(service.calculate());
+    }
+}
---
+++ src/test/java/ru/netology/restMonthCounter/services/restMonthCounterTest.java
@@ -0,0 +1,13 @@
+package ru.netology.restMonthCounter.services;
+
+//import org.junit.jupiter.api.Test;
+
+//public class restMonthCounterTest {
+
+  //  @Test
+    //public void shouldCalcExact() {
+      //  restMonthCounterTest = new restMonthCounterTest;
+
+
+    //}
+//}
